8dee3ffe8093bc4e0341aca48609ac89998a44e6
sunmeng <969945760@qq.com>
2018-06-06 17:00:23 +0800

    hacker June 1 颜色保存

--- src/controller/forecastController/ForecastController.java
+++ src/controller/forecastController/ForecastController.java
@@ -15,29 +15,29 @@ public class ForecastController extends Controller {
     public void doForecast() throws ParseException {
         String taskId=getPara("taskId");
         forecastService.doForecast(taskId);
-        System.out.println(forecastService.forecastC);
+//        System.out.println(forecastService.forecastC);
         setAttr("listViewRain",forecastService.getRainData());
-        setAttr("listChildRainStation",forecastService.getChildRainStation());
-        setAttr("listFractureChild",forecastService.getFractureChild());
-        setAttr("listDayevH",forecastService.getDayevH());
-        setAttr("listSoilCh",forecastService.getSoilCh());
-        setAttr("listXAJFracturePara",forecastService.getFracturePara());
-        setAttr("listXAJChildPara",forecastService.getChildPara());
-        setAttr("listXAJFutureRain",forecastService.getFutureRain());
-        setAttr("listXAJFutureWater",forecastService.getFutureWater());
-        setAttr("e ",forecastService.getE());
-        setAttr("listViewReservoir",forecastService.getReservoir());
-        setAttr("listStrobeFlow",forecastService.getStrobeFlow());
-        setAttr("listHydrologicFlow",forecastService.getHydrologicFlow());
-        setAttr("listXAJMMusk",forecastService.getMMusk());
-        setAttr("listXAJForecastXajr",forecastService.getForecastXajr());
-
-        setAttr("listJYChildRainStation",forecastService.getExperienceChildRainStation());
-        setAttr("listJYFractureChild",forecastService.getExperienceFractureChild());
-        setAttr("listJYChildPara",forecastService.getExperienceChildPara());
-        setAttr("listJYConfig",forecastService.getExperienceConfig());
-        setAttr("listJYHydrologyFlow",forecastService.getHydrologyFlow());
-        setAttr("listParaMu",forecastService.getParaMu());
+        //setAttr("listChildRainStation",forecastService.getChildRainStation());
+//        setAttr("listFractureChild",forecastService.getFractureChild());
+//        setAttr("listDayevH",forecastService.getDayevH());
+//        setAttr("listSoilCh",forecastService.getSoilCh());
+//        setAttr("listXAJFracturePara",forecastService.getFracturePara());
+//        setAttr("listXAJChildPara",forecastService.getChildPara());
+//        setAttr("listXAJFutureRain",forecastService.getFutureRain());
+//        setAttr("listXAJFutureWater",forecastService.getFutureWater());
+//        setAttr("e ",forecastService.getE());
+//        setAttr("listViewReservoir",forecastService.getReservoir());
+//        setAttr("listStrobeFlow",forecastService.getStrobeFlow());
+//        setAttr("listHydrologicFlow",forecastService.getHydrologicFlow());
+//       setAttr("listXAJMMusk",forecastService.getMMusk());
+       // setAttr("listXAJForecastXajr",forecastService.getForecastXajr());
+//
+//        setAttr("listJYChildRainStation",forecastService.getExperienceChildRainStation());
+//        setAttr("listJYFractureChild",forecastService.getExperienceFractureChild());
+//        setAttr("listJYChildPara",forecastService.getExperienceChildPara());
+//        setAttr("listJYConfig",forecastService.getExperienceConfig());
+//        setAttr("listJYHydrologyFlow",forecastService.getHydrologyFlow());
+      //  setAttr("listParaMu",forecastService.getParaMu());
 
         renderJson();
     }
