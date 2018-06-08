78cd0318d0abaec88ab952bf65ac2838d4c301f2
YLirene <981550836@qq.com>
2018-06-08 15:29:44 +0800

    hacker June 8 结果查询

--- src/dao/resultDao/ResultDao.java
+++ src/dao/resultDao/ResultDao.java
@@ -1,6 +1,7 @@
 package dao.resultDao;
 
 import com.jfinal.core.Controller;
+import dao.forecastDao.ForecastResultDao;
 import model.dbmodel.ForecastC;
 import model.dbmodel.ForecastJyt;
 import model.dbmodel.ForecastXajt;
@@ -18,6 +19,7 @@ import java.util.List;
  */
 public class ResultDao extends Controller {
     Date day=new Date();
+    ForecastResultDao forecastResultDao = new ForecastResultDao();
     SimpleDateFormat df = new SimpleDateFormat("yyyyMM");
 
     /**
@@ -27,27 +29,19 @@ public class ResultDao extends Controller {
      */
     public List<ForecastC> getMonthHistory(String userId) {
 //        List<ForecastC> listForecastC=ForecastC.dao.find("select  DISTINCT(no) from f_forecast_c where no like '"+userId+"%"+df.format(day)+"%' order by no desc");
-        List<ForecastC> listForecastC=ForecastC.dao.find("select  DISTINCT(no) from f_forecast_c where no like '"+userId+"%' order by no desc");
+        List<ForecastC> listForecastC=ForecastC.dao.find("select  DISTINCT(no),basedtm,starttm,endtm from f_forecast_c where no like '"+userId+"%' order by no desc");
         return listForecastC;
     }
 
     /**
-     * 新安江模型最近一次预报查询
+     * 获取任务参数
      * @param taskId
      * @return
      */
-    public List<ForecastXajt> getMonthHistoryXajtLatest(String taskId) {
-        return ForecastXajt.dao.find("select * from f_forecast_xajt where no=?",taskId);
+    public ForecastC getTaskSetting(String taskId) {
+        return forecastResultDao.getTaskSetting(taskId);
     }
 
-    /**
-     * 经验模型最近一次预报查询
-     * @param taskId
-     * @return
-     */
-    public List<ForecastJyt> getMonthHistoryJytLatest(String taskId) {
-       return ForecastJyt.dao.find("select * from f_forecast_jyt where no=?",taskId);
-    }
 
     /**
      * 根据用户获取任务编号
@@ -67,45 +61,8 @@ public class ResultDao extends Controller {
      * @return
      */
     public List<ForecastC> getSearchHistory(String userId, String searchText) {
-        List<ForecastC> listForecastC=ForecastC.dao.find("select  DISTINCT(no) from f_forecast_c where no like '"+userId+"%"+searchText+"%' order by no desc");
+        List<ForecastC> listForecastC=ForecastC.dao.find("select  DISTINCT(no),jno,basedtm,starttm,endtm from f_forecast_c where no like '"+userId+"%"+searchText+"%' order by no desc");
         return listForecastC;
-    }
-
-    /**
-     *点击搜索获取新安江模型最近一次预报结果
-     * @param taskId
-     * @return
-     */
-    public List<ForecastXajt> getSearchHistoryXajtLatest(String taskId) {
-        return ForecastXajt.dao.find("select * from f_forecast_xajt where no=?",taskId);
-    }
-
-    /**
-     * 点击搜索获取经验模型最近一次预报结果
-     * @param taskId
-     * @return
-     */
-    public List<ForecastJyt> getSearchHistoryJytLatest(String taskId) {
-        return ForecastJyt.dao.find("select * from f_forecast_jyt where no=?",taskId);
-    }
-
-    /**
-     * 根据任务编号获取数据
-     * @param taskId
-     * @return
-     */
-    public List<ForecastC> getHistoryByTaskId(String taskId) {
-        List<ForecastC> listForecastC=ForecastC.dao.find("select * from f_forecast_c where no=?",taskId);
-        return listForecastC;
-    }
-
-    /**
-     * 根据任务编号获取新安江模型数据
-     * @param taskId
-     * @return
-     */
-    public List<ForecastXajt> getHistoryByTaskIdXajt(String taskId) {
-        return ForecastXajt.dao.find("select * from f_forecast_xajt where no=?",taskId);
     }
 
     /**
@@ -113,8 +70,9 @@ public class ResultDao extends Controller {
      * @param taskId
      * @return
      */
-    public List<ForecastJyt> getHistoryByTaskIdJyt(String taskId) {
-        return ForecastJyt.dao.find("select * from f_forecast_jyt where no=?",taskId);
+    public List<JYForecastJyt> getHistoryByTaskIdJyt(String taskId) {
+        ForecastC forecastC = forecastResultDao.getTaskSetting(taskId);
+        return forecastResultDao.getForecastJyt(forecastC.getJNO());
     }
 
     /**
@@ -123,16 +81,7 @@ public class ResultDao extends Controller {
      * @return
      */
     public List<XAJForecastXajt> getXajName(String taskId) {
-        List<XAJForecastXajt> listXAJForecastXajt=new ArrayList<XAJForecastXajt>();
-        List<Tree> listFracture=Tree.dao.find("select * from f_tree where rank=2 and pid like '001%'");
-        for(Tree fracture:listFracture){
-            List<ForecastXajt> listForecastXajt=ForecastXajt.dao.find("select * from f_forecast_xajt where no=?",taskId);
-            XAJForecastXajt xajtName=new XAJForecastXajt();
-            xajtName.setFractureId(fracture.getID());
-            xajtName.setFractureName(fracture.getNAME());
-            listXAJForecastXajt.add(xajtName);
-        }
-        return listXAJForecastXajt;
+        return forecastResultDao.getForecastXajt(taskId);
     }
 
     /**
@@ -141,16 +90,7 @@ public class ResultDao extends Controller {
      * @return
      */
     public List<JYForecastJyt> getJyName(String taskJid) {
-        List<JYForecastJyt> listJYForecastJyt=new ArrayList<JYForecastJyt>();
-        List<Tree> listFracture=Tree.dao.find("select * from f_tree where rank=2 and pid like '101%'");
-        for(Tree fracture:listFracture){
-            List<ForecastJyt> listForecastJyt=ForecastJyt.dao.find("select * from f_forecast_xajt where no=?",taskJid);
-            JYForecastJyt jyName=new JYForecastJyt();
-            jyName.setFractureId(fracture.getID());
-            jyName.setFractureName(fracture.getNAME());
-            listJYForecastJyt.add(jyName);
-        }
-        return listJYForecastJyt;
+        return forecastResultDao.getForecastJyt(taskJid);
     }
 }
 
